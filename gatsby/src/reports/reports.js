54fb11a149b2408d5e438bfdda00bc48b9288457
Alex Fornuto <alex@fornuto.com>
2020-04-02 14:54:55 -0500

    init sorting function
    If anyone every picks this back up, it follows the examples here: https://www.smashingmagazine.com/2020/03/sortable-tables-react/

--- gatsby/src/reports/reports.js
+++ gatsby/src/reports/reports.js
@@ -69,7 +69,10 @@ class ReviewReport extends React.Component {
           }
         `}
         render={data => {
+          // Constants creating Hooks
           const [search, setSearch] = useState("")
+
+          // Constants creating arrays from the GraphQL Query
           const reviewedPages = data.allReviewedDocs.edges
           const reviewedTertiaryPages = data.allReviewedDocs.edges.filter(
             page => {
@@ -85,7 +88,44 @@ class ReviewReport extends React.Component {
               return page.node.fields.slug.match(/\/guides(\/[a-z,\-]*){2}/)
             }
           )
-          console.log("Unreviewed Tertiary Pages: ", unreviewedTertiaryPages) // For debugging
+          //console.log("Unreviewed Tertiary Pages: ", unreviewedTertiaryPages) // For debugging
+
+          // Sorting Function
+          const useSortableData = (items, config = null) => {
+            const [sortConfig, setSortConfig] = React.useState(config);
+
+            const sortedItems = React.useMemo(() => {
+              let sortableItems = [...items];
+              if (sortConfig !== null) {
+                sortableItems.sort((a, b) => {
+                  if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                  }
+                  if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                  }
+                  return 0;
+                });
+              }
+              return sortableItems;
+            }, [items, sortConfig]);
+
+            const requestSort = (key) => {
+              let direction = 'ascending';
+              if (
+                sortConfig &&
+                sortConfig.key === key &&
+                sortConfig.direction === 'ascending'
+              ) {
+                direction = 'descending';
+              }
+              setSortConfig({ key, direction });
+            };
+
+            return { items: sortedItems, requestSort, sortConfig };
+          };
+
+
           return (
             <Layout>
               <h1>Reports</h1>
@@ -130,7 +170,11 @@ class ReviewReport extends React.Component {
                     <br />
                     <thead>
                       <tr>
-                        <th width="40%">Title</th>
+                        <th width="40%">
+                          <a href="#" onClick={() => setSortedField('title')}>
+                          Title
+                          </a>
+                        </th>
                         <th width="20%">Path</th>
                         <th>Date</th>
                       </tr>
