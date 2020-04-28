e66d08d7621ddde37f15beefdfe3afe0c9477d7d
Alex Fornuto <alex@fornuto.com>
2020-04-28 16:11:23 -0500

    attempt to use simpler logic, currently failing

--- src/components/tabList/index.js
+++ src/components/tabList/index.js
@@ -3,7 +3,7 @@ import Tab from "../tab"
 import "./style.css"
 
 const TabList = ({ children }) => {
-  var cmsProp = "Drupal 8" // To be set by another component, yet to be created
+  var cmsProp  = "Drupal 7"// To be set by another component, yet to be created
   //console.log("cmsProp: ", cmsProp) // For Debugging
   const [activeTab, setActiveTab] = useState(null)
   const [initialized, setInitialized] = useState(null)
@@ -16,18 +16,27 @@ const TabList = ({ children }) => {
   useEffect(() => {
     if (!initialized) {
       // determine which tab is initially active
-      const initialActiveTab = isCmsTabs
-        ? children.find(
-            tab => tab.props.title.toLowerCase() == selectedCms.toLowerCase()
-          ) ||
-          children.find(tab =>
-            tab.props.title.includes(selectedCms.substring(0, 6))
+      console.log("isCmsTabs? ", isCmsTabs) // For Debugging
+      //debugger
+      const initialActiveTab = isCmsTabs && selectedCms
+        ?
+        children.find(
+            tab => {
+              console.log("tab.props.title.toLowerCase(): ", tab.props.title.toLowerCase())
+              console.log("selectedCms.toLowerCase(): ", selectedCms.toLowerCase())
+              tab.props.title.toLowerCase() == selectedCms.toLowerCase()
+              }
+          ) ^
+          children.find(tab => {
+            console.log("tab.props.title.toLowerCase(): ", tab.props.title.toLowerCase())
+              console.log("selectedCms.toLowerCase().substring(0, 6): ", selectedCms.toLowerCase().substring(0, 6))
+            tab.props.title.toLowerCase().includes(selectedCms.toLowerCase().substring(0, 6))
+          }
           )
         : children.find(tab => tab.props.active === true)
-      //console.log("initialActiveTab: ", initialActiveTab) // For Debugging
+      console.log("initialActiveTab: ", initialActiveTab) // For Debugging
       initialActiveTab && setActiveTab(initialActiveTab.props.id)
-
-      setInitialized(true)
+      //setInitialized(true)
     }
   }, [isCmsTabs])
 
@@ -38,27 +47,31 @@ const TabList = ({ children }) => {
     //console.log("titles array: ", titles) // For Debugging
 
     function findCommonElement(array1, array2) {
-      // Loop for array1
-      for (let i = 0; i < array1.length; i++) {
-        // Loop for array2
-        for (let j = 0; j < array2.length; j++) {
-          // Compare the element of each and
-          // every element from both of the
-          // arrays
-          if (array2[j].toLowerCase().indexOf(array1[i]) > -1) {
-            // Return if common element found
-            return true
-          }
-        }
-      }
-      // Return if no common element exist
-      return false
+      return array1.some(item =>
+        array2.includes(item.toLowerCase().substring(0, 6))
+      )
+      //      // Loop for array1
+      //      for (let i = 0; i < array1.length; i++) {
+      //        // Loop for array2
+      //        for (let j = 0; j < array2.length; j++) {
+      //          // Compare the element of each and
+      //          // every element from both of the
+      //          // arrays
+      //          if (array2[j].toLowerCase().indexOf(array1[i]) > -1) {
+      //            // Return if common element found
+      //            return true
+      //          }
+      //        }
+      //      }
+      //      // Return if no common element exist
+      //      return false
     }
 
     if (titles.length) {
-      const hasIntersection = findCommonElement(cmses, titles)
+      const hasIntersection = findCommonElement(titles, cmses)
       //console.log("value of hasIntersection: ", hasIntersection) // For debugging
       setIsCmsTabs(hasIntersection)
+      //console.log("just ran setIsCmsTabs, value: ", isCmsTabs) // For Debugging
     } else {
       setIsCmsTabs(false)
     }
@@ -116,11 +129,11 @@ const TabList = ({ children }) => {
   return (
     <>
       <ul className="nav nav-tabs" role="tablist">
-        {isCmsTabs
+        {isCmsTabs && selectedCms
           ? children
               .filter(tab => {
-                ///console.log("tab.props.title.includes(selectedCms: )", tab.props.title.includes(selectedCms.substring(0,6))) // For debuggind
-                return tab.props.title.indexOf(selectedCms.substring(0, 6)) >= 0
+                ///console.log("tab.props.title.includes(selectedCms: )", tab.props.title.includes(selectedCms.substring(0,6))) // For debugging
+                return tab.props.title.toLowerCase().indexOf(selectedCms.toLowerCase().substring(0, 6)) >= 0
               })
               .map(tab => renderTab(tab))
           : children.map(tab => renderTab(tab))}
